00be9328832110894ce987e1805a39818b1c2e19
Shivam Kashyap <sk8810624267@gmail.com>
2024-07-02 20:06:12 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let typed = new Typed(".msg", {
+    strings: ["Frontend Developer", "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
